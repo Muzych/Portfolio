f6b7ddd73aa47e6cd8b813369772aad9d72ce375
Li0329 <haxk1024@gmail.com>
2024-02-03 20:47:54 +0800

    feat: update left-panel

---
+++ components/config/site-config.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+    creator: "Muzych",
+    title: "Indie Hacker",
+    bio: "Software Engineer | Full Stack, Computer Graphics",
+    location: "China",
+    locationLink: "",
+    email: "haxk1024@gmail.com",
+} as const;
--- components/left-panel.tsx
+++ components/left-panel.tsx
@@ -1,5 +1,34 @@
-export default function LeftPanel(){
+"use client"
+import Image from "next/image"
+import { siteConfig } from "./config/site-config"
+
+const LeftPanel = () => {
     return (
-        <div>I am left panel</div>
+        <div
+            key="left-panel"
+            className="flex flex-col justify-between py-6 xl:max-w-sm xl:py-10 xl:h-full"
+        >
+            <div>
+                <div>
+                    <Image 
+                      priority
+                      loading="eager"
+                      alt="avatar"
+                      src="/wave.png"    //Todo:更新avatar
+                      width={220}
+                      height={220}
+                    />
+                </div>
+
+                <div className="mt-6">
+                    <div className="text-xl font-medium text-primary">
+                        {siteConfig.title}
+                    </div>
+                    <h1 className="mt-2 text-4xl font-bold">{siteConfig.creator}</h1>
+                </div>
+            </div>
+        </div>
     )
 }
+
+export default LeftPanel
