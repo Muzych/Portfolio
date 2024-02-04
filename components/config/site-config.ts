c6c4e4dc684e33e068375822b4183306f52e43b1
Li0329 <haxk1024@gmail.com>
2024-02-04 17:06:03 +0800

    feat: add the grid-items/equipment

--- components/config/site-config.ts
+++ components/config/site-config.ts
@@ -1,3 +1,239 @@
+export type GridItemLayout = "1x2" | "2x1" | "2x2" | "2x4" | "4x4";
+export type GridItemType = "social" | "equipment" | "mentor" | "project" | "experience" | "skills"
+export type EquipmentItem = {
+    title: string;
+    link: string;
+}
+
+
+
+export interface GridItemInterface {
+    layout: GridItemLayout;
+    type: GridItemType;
+    title: string;
+    icon?: string;
+    username?: string;
+    description?: string;
+    color?: string;
+    buttonTitle?: string;
+    buttonLink?: string;
+    buttonSecondaryText?: string;
+    /* Mentor */
+    promotion?: string;
+    price?: string;
+    oldPrice?: string;
+    /* Project */
+    stars?: number;
+    /* Equipments */
+    equipments?: EquipmentItem[];
+    image?: string;
+}
+
+const GridItemsLower: GridItemInterface[] = [
+    // {
+    //     layout: "2x2",
+    //     type: "social",
+    //     title: "LinkedIn",
+    //     icon: "linkedin",
+    //     username: "michel-fawaz",
+    //     description:
+    //         "Connect with me on LinkedIn to explore my professional journey and stay updated on my latest achievements and projects showcased in my portfolio website.",
+    //     color: "#0077b5",
+    //     buttonTitle: "Connect",
+    //     buttonLink: "https://www.linkedin.com/in/michel-fawaz/",
+    //     // buttonSecondaryText: "1.1K",
+    // },
+    //   {
+    //     layout: "1x2",
+    //     type: "social",
+    //     title: "Discord",
+    //     icon: "discord",
+    //     username: "@makrdev",
+    //     buttonTitle: "Join",
+    //     buttonLink: "https://discord.com/invite/5aXRp49Q4h",
+    //     buttonSecondaryText: "44",
+    //     color: "#5865F2",
+    //   },
+    // {
+    //     layout: "2x2",
+    //     type: "social",
+    //     title: "Github",
+    //     icon: "github",
+    //     username: "@michelfawazz",
+    //     buttonTitle: "Follow",
+    //     buttonSecondaryText: "",
+    //     buttonLink: "https://github.com/michelfawazz",
+    //     color: "#070707",
+    //     description:
+    //         "Click on me to view my open-source projects!",
+    // },
+    // {
+    //     layout: "1x2",
+    //     type: "social",
+    //     title: "Twitter",
+    //     icon: "twitter",
+    //     username: "@michelNfawaz",
+    //     buttonTitle: "Follow",
+    //     buttonLink: "https://twitter.com/michelnfawaz",
+    //     buttonSecondaryText: "193",
+    //     color: "#1DA1F2",
+    //     description:
+    //         "Software Engineer building in Public!",
+    // },
+    // {
+    //     layout: "1x2",
+    //     type: "social",
+    //     title: "Resume",
+    //     icon: "resume",
+    //     username: "",
+    //     buttonTitle: "Download",
+    //     buttonLink: "https://utfs.io/f/f3db2131-47fe-47f2-a80b-6fa0bbea97a9-8a3mok.pdf",
+    //     buttonSecondaryText: "",
+    //     color: "#070707",
+    //     description:
+    //         "Download My resume!",
+    // },
+    // {
+    //     layout: "2x1",
+    //     type: "project",
+    //     title: "Sign in with Ethereum",
+    //     icon: "github",
+    //     color: "#070707",
+    //     buttonLink: "https://github.com/michelfawazz/SIWE-NextJs-Nestjs",
+    //     stars: 1,
+    // },
+    // {
+    //     layout: "2x1",
+    //     type: "mentor",
+    //     title: "Next.js Mentorship",
+    //     icon: "superpeer",
+    //     promotion: "MAKRDEVFELLOWS",
+    //     oldPrice: "$100",
+    //     price: "$50",
+    //     buttonLink: "https://superpeer.com/makrdev/-/nextjs-mentor",
+    // },
+    // {
+    //     layout: "2x1",
+    //     type: "mentor",
+    //     title: "Supabase Mentorship",
+    //     icon: "superpeer",
+    //     promotion: "MAKRDEVFELLOWS",
+    //     oldPrice: "$100",
+    //     price: "$50",
+    //     buttonLink: "https://superpeer.com/makrdev/-/supabase-mentor",
+    // },
+
+    {
+        layout: "1x2",
+        type: "social",
+        title: "InfinityTools",
+        username: "",
+        description:
+            "A collection of AI tools",
+        icon: "next",
+        buttonTitle: "Coming Soon!",
+        buttonSecondaryText: "",
+        buttonLink:
+            "/",
+    },
+
+    
+    {
+        layout: "2x2",
+        type: "social",
+        title: "Restaurant Web3 Membership System NFT ",
+        username: "Le Vesuvio",
+        description:
+            "NFT collection and membership website made for Le Vesuvio restaurant.",
+        icon: "next",
+        buttonTitle: "View Project",
+        buttonSecondaryText: "",
+        buttonLink:
+            "https://vesuv.io/",
+    },
+    {
+        layout: "1x2",
+        type: "social",
+        title: "AI Agency",
+        username: "",
+        description:
+            "Generate Ai employees that you can Chat with!",
+        icon: "next",
+        buttonTitle: "Coming Soon!",
+        buttonSecondaryText: "",
+        buttonLink:
+            "/",
+    },
+    {
+        layout: "2x1",
+        type: "project",
+        title: "KnowledgeBase Chat AI Saas ",
+        icon: "github",
+        stars: 1,
+        color: "#070707",
+        buttonLink: "https://github.com/",
+    }
+    ,{
+        layout: "2x2",
+        type: "social",
+        title: "AktionT4 NFT Collection & Backend",
+        username: "",
+        description:
+            "Aktion T4 Blockchain and Backend Development",
+        icon: "ethereum",
+        buttonTitle: "View Project",
+        buttonSecondaryText: "",
+        buttonLink:
+            "https://aktionT4.com",
+    },
+    ,{
+        layout: "1x2",
+        type: "social",
+        title: "NFT GAME 1024",
+        username: "",
+        description:
+            "Smart Contract and Website development of 1024 Blockchain Game",
+        icon: "ethereum",
+        buttonTitle: "View Project",
+        buttonSecondaryText: "",
+        buttonLink:
+            "1024-landingpage-tan.vercel.app",
+    },
+    {
+        layout: "1x2",
+        type: "social",
+        title: "Priority Queue Management System",
+        username: "QueueUp",
+        description:
+            "Priority Queue management system built with django",
+        icon: "django",
+        buttonTitle: "View Project",
+        buttonSecondaryText: "",
+        buttonLink:
+            "https://github.com/michelfawazz/QueueUp",
+    },
+    {
+        layout: "2x1",
+        type: "project",
+        title: "Portfolio Website Source Code",
+        icon: "github",
+        stars: 1,
+        color: "#070707",
+        buttonLink: "https://github.com/michelfawazz/Portfolio-Bento-style",
+    },
+
+    {
+        layout:"4x4",
+        type: "skills",
+        title: "Skills",
+    },
+    {
+        layout: "2x4",
+        type: "experience",
+        title: "Professional Experience"
+    }
+];
+
 export const siteConfig = {
     creator: "Muzych",
     title: "Indie Hacker",
@@ -5,4 +241,5 @@ export const siteConfig = {
     location: "China",
     locationLink: "",
     email: "haxk1024@gmail.com",
+    itemsLower: GridItemsLower,
 } as const;
--- components/mid-panel.tsx
+++ components/mid-panel.tsx
@@ -1,5 +1,25 @@
-export default function MidPanel(){
-    return (
-        <div>I am mid panel</div>
+"use client"
+import { siteConfig } from "@/components/config/site-config";
+import GridItem from "./grid-item";
+import Equipments from "./grid-items/equipments";
+
+
+const MidPanel = () => {
+    return   (
+        <div
+            className="grid w-full grid-cols-4 xl:gap-10 gap-6"
+        >
+            {siteConfig.itemsLower.map((item, index) => {
+                return (
+                    <GridItem key={item.title + item.type + index} size={item.layout}>
+                        {item.type === "equipment" ? (<Equipments item={item} />) : (
+                                    <div>Need to create new component type.</div>
+                                )}
+                    </GridItem>
+                )
+            })}
+        </div>
     )
 }
+
+export default MidPanel
