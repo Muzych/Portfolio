092305fe7a86bf9c7311cda66e099217e112fc99
Li0329 <haxk1024@gmail.com>
2024-02-05 17:53:17 +0800

    feat: delete the comma

--- components/config/site-config.ts
+++ components/config/site-config.ts
@@ -43,17 +43,6 @@ const GridItems: GridItemInterface[] = [
         buttonLink: "https://www.linkedin.com/in/michel-fawaz/",
         // buttonSecondaryText: "1.1K",
     },
-    //   {
-    //     layout: "1x2",
-    //     type: "social",
-    //     title: "Discord",
-    //     icon: "discord",
-    //     username: "@makrdev",
-    //     buttonTitle: "Join",
-    //     buttonLink: "https://discord.com/invite/5aXRp49Q4h",
-    //     buttonSecondaryText: "44",
-    //     color: "#5865F2",
-    //   },
     {
         layout: "2x2",
         type: "social",
@@ -125,159 +114,11 @@ const GridItems: GridItemInterface[] = [
         stars: 2,
 
         buttonLink: "https://github.com/michelfawazz/NestJS-Jwt-AUTH-arangodb",
-    },
-    // {
-    //     layout: "2x1",
-    //     type: "mentor",
-    //     title: "Supabase Mentorship",
-    //     icon: "superpeer",
-    //     promotion: "MAKRDEVFELLOWS",
-    //     oldPrice: "$100",
-    //     price: "$50",
-    //     buttonLink: "https://superpeer.com/makrdev/-/supabase-mentor",
-    // },
-    
-    // {
-    //     layout: "2x2",
-    //     type: "social",
-    //     title: "Restaurant Web3 Membership System NFT ",
-    //     username: "Le Vesuvio",
-    //     description:
-    //         "NFT collection and membership website made for Le Vesuvio restaurant.",
-    //     icon: "next",
-    //     buttonTitle: "View Project",
-    //     buttonSecondaryText: "",
-    //     buttonLink:
-    //         "https://vesuv.io/",
-    // },
-    // {
-    //     layout: "2x1",
-    //     type: "project",
-    //     title: "AI Blog Post Generator",
-    //     icon: "github",
-    //     stars: 19,
-    //     color: "#070707",
-    //     buttonLink: "https://github.com/batuhanbilginn/ai-blog-post-generator",
-    // }
-    // ,{
-    //     layout: "2x2",
-    //     type: "social",
-    //     title: "AktionT4 NFT Collection & Backend",
-    //     username: "",
-    //     description:
-    //         "Aktion T4 Blockchain and Backend Development",
-    //     icon: "ethereum",
-    //     buttonTitle: "View Project",
-    //     buttonSecondaryText: "",
-    //     buttonLink:
-    //         "https://aktionT4.com",
-    // },
-    // },
-    // {
-    //     layout: "2x4",
-    //     type: "experience",
-    //     title: "Professional Experience",
-        
- 
-        
- 
+    }
 ];
 
 const GridItemsLower: GridItemInterface[] = [
-    // {
-    //     layout: "2x2",
-    //     type: "social",
-    //     title: "LinkedIn",
-    //     icon: "linkedin",
-    //     username: "michel-fawaz",
-    //     description:
-    //         "Connect with me on LinkedIn to explore my professional journey and stay updated on my latest achievements and projects showcased in my portfolio website.",
-    //     color: "#0077b5",
-    //     buttonTitle: "Connect",
-    //     buttonLink: "https://www.linkedin.com/in/michel-fawaz/",
-    //     // buttonSecondaryText: "1.1K",
-    // },
-    //   {
-    //     layout: "1x2",
-    //     type: "social",
-    //     title: "Discord",
-    //     icon: "discord",
-    //     username: "@makrdev",
-    //     buttonTitle: "Join",
-    //     buttonLink: "https://discord.com/invite/5aXRp49Q4h",
-    //     buttonSecondaryText: "44",
-    //     color: "#5865F2",
-    //   },
-    // {
-    //     layout: "2x2",
-    //     type: "social",
-    //     title: "Github",
-    //     icon: "github",
-    //     username: "@michelfawazz",
-    //     buttonTitle: "Follow",
-    //     buttonSecondaryText: "",
-    //     buttonLink: "https://github.com/michelfawazz",
-    //     color: "#070707",
-    //     description:
-    //         "Click on me to view my open-source projects!",
-    // },
-    // {
-    //     layout: "1x2",
-    //     type: "social",
-    //     title: "Twitter",
-    //     icon: "twitter",
-    //     username: "@michelNfawaz",
-    //     buttonTitle: "Follow",
-    //     buttonLink: "https://twitter.com/michelnfawaz",
-    //     buttonSecondaryText: "193",
-    //     color: "#1DA1F2",
-    //     description:
-    //         "Software Engineer building in Public!",
-    // },
-    // {
-    //     layout: "1x2",
-    //     type: "social",
-    //     title: "Resume",
-    //     icon: "resume",
-    //     username: "",
-    //     buttonTitle: "Download",
-    //     buttonLink: "https://utfs.io/f/f3db2131-47fe-47f2-a80b-6fa0bbea97a9-8a3mok.pdf",
-    //     buttonSecondaryText: "",
-    //     color: "#070707",
-    //     description:
-    //         "Download My resume!",
-    // },
-    // {
-    //     layout: "2x1",
-    //     type: "project",
-    //     title: "Sign in with Ethereum",
-    //     icon: "github",
-    //     color: "#070707",
-    //     buttonLink: "https://github.com/michelfawazz/SIWE-NextJs-Nestjs",
-    //     stars: 1,
-    // },
-    // {
-    //     layout: "2x1",
-    //     type: "mentor",
-    //     title: "Next.js Mentorship",
-    //     icon: "superpeer",
-    //     promotion: "MAKRDEVFELLOWS",
-    //     oldPrice: "$100",
-    //     price: "$50",
-    //     buttonLink: "https://superpeer.com/makrdev/-/nextjs-mentor",
-    // },
-    // {
-    //     layout: "2x1",
-    //     type: "mentor",
-    //     title: "Supabase Mentorship",
-    //     icon: "superpeer",
-    //     promotion: "MAKRDEVFELLOWS",
-    //     oldPrice: "$100",
-    //     price: "$50",
-    //     buttonLink: "https://superpeer.com/makrdev/-/supabase-mentor",
-    // },
-
-    ,{
+ {
         layout: "1x2",
         type: "social",
         title: "InfinityTools",
@@ -290,8 +131,6 @@ const GridItemsLower: GridItemInterface[] = [
         buttonLink:
             "/",
     },
-
-    
     {
         layout: "2x2",
         type: "social",
@@ -340,7 +179,7 @@ const GridItemsLower: GridItemInterface[] = [
         buttonLink:
             "https://aktionT4.com",
     },
-    ,{
+    {
         layout: "1x2",
         type: "social",
         title: "NFT GAME 1024",
@@ -375,17 +214,6 @@ const GridItemsLower: GridItemInterface[] = [
         color: "#070707",
         buttonLink: "https://github.com/michelfawazz/Portfolio-Bento-style",
     }
-
-    // {
-    //     layout:"4x4",
-    //     type: "skills",
-    //     title: "Skills",
-    // }
-    // },
-    // {
-    //     layout: "2x4",
-    //     type: "experience",
-    //     title: "Professional Experience",
 ];
 
 export const siteConfig = {
